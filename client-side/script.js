c544f1549b0e03993f9e7f38e693046f10fbde8e
mehulcs <mehul.becs@gmail.com>
2017-04-29 14:26:47 +0530

    new features

--- client-side/script.js
+++ client-side/script.js
@@ -1,113 +1,238 @@
 
-var socket = io("https://hidden-waters-71530.herokuapp.com/");
-var roomnumber,data=[1111,'a'],arr = [],box=[];
+var socket = io("http://192.168.0.105:3000");
+var roomnumber,data=[1111,'a'],arr = [],box=[],name,chance=0,last="";
 
-function playarea(){
-    // document.body.innerHTML = '<p>show bingo box</p>'+
-    // '<button onclick="sendmessage()">Send to members</button>';
-    document.body.innerHTML = '<div w3-include-html="content.html"></div>';
-    
-    w3IncludeHTML();
-    assignid();
+
+
+// ------------registration----------------------------------------------
+function register(event){ //read enter press
+    var code=event.keyCode;
+    if(code == 13){
+        event.preventDefault();
+        registername();
+    }
 }
+function registername(){  
+    name=document.getElementById('register-name').value;
+    if(name == ""){
+        alert("Please enter name");
+        return false;
+    }
+
+    $(document.body).load("main.html");
+}
+function registerguest(){
+    var val = Math.floor(1000 + Math.random() * 9000);
+    name="guest"+val;
+  
+    $(document.body).load("main.html");
+}
+//----------------------------------------------------------------------------
+
+function leave(){
+    data[0]=roomnumber;
+    data[1]=name;
+    
+    if(chance==1){
+        socket.emit('nextchanceonleave',data);
+    }
+    socket.emit('leaveroom',data);
+    $(document.body).load("main.html");
+    chance=0;
+
+}
+
+function playarea(callback){
+    $(document.body).load("bingo.html",function(){
+        assignid();
+        callback();
+    });
+
+}
+
+
+
+// ----------createroom------------------------------------------------
 function createroom(){   
     roomnumber = new Date().valueOf()%10000;
-    socket.emit('createroom',roomnumber);    
-    playarea();
+    data[0]=roomnumber;
+    data[1]=name;
+    socket.emit('createroom',data);    
+    
 }
+socket.on('createres',function(mes){
+    playarea(function(){
+        var list=document.getElementById("users");
+        document.getElementById("roomstatus").innerHTML="Room Number "+mes;
+        list.innerHTML=list.innerHTML+'<div id="'+name+'" class="userlist"><p class="user">'+name+'</p></div>';
+    });
+    chance=1;
+});
+
+//----------joinroom-------------------------------------------------
 function joinroom(){
     roomnumber=document.getElementById('input_room').value;
     if (roomnumber==""){  
         alert("Please Enter Room Number");  
         return false;  
     }else{
-        socket.emit('joinroom',roomnumber);
-        playarea();
+        data[0]=roomnumber;
+        data[1]=name;
+        socket.emit('joinroom',data);
     }
 }
-function sendmessage(boxnumber){
-    data[0]=roomnumber;
-    data[1]=boxnumber;
-    socket.emit('forroommembers',data);
+socket.on('joinres',function(users){
+    playarea(function(){
+        document.getElementById("roomstatus").innerHTML="Room Number "+roomnumber;
+        var list=document.getElementById("users"),lists="";
+        for(var i=0;i<users.length;i++){
+            lists += '<div id="'+users[i]+'" class="userlist"><p class="user">'+users[i]+'</p></div>';
+        }
+        list.innerHTML=lists;
+    });
+});
+socket.on('users',function(users){
+    var list=document.getElementById("users"),lists="";
+    for(var i=0;i<users.length;i++){
+        if(users[i]==name&&chance==1){
+            lists += '<div id="'+users[i]+'" class="userlist" style="border:2px solid #c554d8;"><p class="user">'+users[i]+'</p></div>';
+        }else{
+            lists += '<div id="'+users[i]+'" class="userlist"><p class="user">'+users[i]+'</p></div>';
+        }
+    }
+    list.innerHTML=lists;
+});
+socket.on('refusejoin',function(room){
+    alert("room "+room+ " doesn't exist");
+    document.getElementById('input_room').value="";
+});
+
+
+//-----------bingobox inputs---------------------------------------------------
+function bingomessage(boxnumber){
+    if(chance==1){
+        data[0]=roomnumber;
+        data[1]=boxnumber;
+        data[2]=name;    
+        if(document.getElementById(boxnumber).style.backgroundColor=="white"){
+            socket.emit('bingoinput',data);
+            document.getElementById(boxnumber).style.backgroundColor="#ffb7c7";
+            document.getElementById(name).style.border="2px solid lightblue";
+            chance=0;
+        }else{
+            $('#myModal').modal('toggle');
+            document.getElementById('alert').innerText="Box selected already..!";
+        }
+    }else{
+        $('#myModal').modal('toggle');
+        document.getElementById('alert').innerText="Please wait for your chance";
+    }
+}
+socket.on('bingoinput',function(input){
+    document.getElementById(input).style.backgroundColor="lightgreen";
+});
+socket.on('nextchance',function(data){
+    chance=1;
+    // document.getElementById(data).style.border="2px solid #c554d8";
+});
+socket.on('showuserschance',function(data){
+    if(last!=""){
+        document.getElementById(last).style.border="2px solid lightblue";
+    }
+    last=data;
+    document.getElementById(data).style.border="2px solid #c554d8";
+});
+
+//--------------------------------------------------------------------------
+
+function resetgame(){
+    socket.emit('resetconfirm',roomnumber);
+}
+socket.on('resetcount',function(){
+    $('#modal2').modal('toggle');
+});
+function confirmreset(){  //called on click yes
+    socket.emit('count',roomnumber);
 }
 
-socket.on('nextinput',function(input){
-    document.getElementById(input).style.backgroundColor="lightgreen";
-    // document.getElementById(input).style.color="white";
-});
-socket.on('createres',function(mes){
-    setTimeout(function() {
-        document.getElementById("roomstatus").innerHTML="Room Number "+mes;
-    }, 2500);
-    
-});
-socket.on('joinres',function(mes){
-    setTimeout(function() {
-        document.getElementById("roomstatus").innerHTML="Room Number "+mes;
-    }, 2500);
-});
+
 socket.on('resetgame',function(data){
     arr=[];
-    getarray();
-    console.log(arr);
-    setTimeout(function(){
+    getarray(function(){
         for(var i=0;i<25;i++){
             box[i].innerHTML=arr[i];
             box[i].id=arr[i];
             box[i].style.backgroundColor="white";
         }
-    },1500);
+    });
 });
-function getarray(){
-    
+
+
+function getarray(callback){
+    arr=[];
     while(arr.length < 25){
         var randomnumber = Math.ceil(Math.random()*25)
         if(arr.indexOf(randomnumber) > -1) continue;
         arr[arr.length] = randomnumber;
+        if(arr.length==25){
+            callback();
+        }
     }
-    
 }
 function assignid(){
     box = document.getElementsByTagName('td');
-
-    getarray();
-
-    setTimeout(function(){
+    getarray(function(){
         for(var i=0;i<25;i++){
             box[i].innerHTML=arr[i];
             box[i].id=arr[i];
+            box[i].style.backgroundColor="white";
         }
-    },1500);
-    
+    });
 }
-function resetgame(){
-    socket.emit('resetgame',roomnumber);
-}
+
+
+
+
+//-------------------------------chatroom----------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+
 function chatmessage(){
+    //sender
     var div = document.getElementById('chatbox');
     var mes=document.getElementById('input-chat').value,data=[];
-    
     setTimeout(scrolldown,500);
     if(mes == ""){
         alert("Please Enter Message");  
+        document.getElementById("input-chat").focus();
         return false;
     }else{
         data[0]=roomnumber;
         data[1]=mes;
+        data[2]=name;
         socket.emit('chatmessage',data);
         div.innerHTML = div.innerHTML + '<div class="message-box-sender" style="float:right;"><p class="message">'+mes+'</p></div><br>';
     }
     document.getElementById('input-chat').value="";
-}
-socket.on('chatmessage',function(message){
     
-    document.getElementById('input-chat').value="";
-    var div = document.getElementById('chatbox');
-    div.innerHTML = div.innerHTML + '<div class="message-box" style="float:left;"><p class="message">'+message+'</p></div><br>';
+}
+socket.on('chatmessage',function(data){
+    //receiver
+    var div = document.getElementById('chatbox'); 
+    div.innerHTML = div.innerHTML + '<div style="clear: both;"><div class="message-box triangle" style="float:left;background-color: rgba(202, 129, 195, 0.75);"><p class="message">'+data[2]+'</p></div><div class="message-box" style="float:left;"><p class="message">'+data[1]+'</p></div><br></div>';
     setTimeout(scrolldown,500);
-
 });
+
+
 function scrolldown(){
     var div=document.getElementById('chatbox');
     div.scrollTop = div.scrollHeight;
 }
+
+function enterbutton(event){
+    var code=event.keyCode;
+    if(code == 13){
+        event.preventDefault();
+        chatmessage();
+    }
+}
+//-----------------------------------------------------------------------------------------------------------
--- server-side/server.js
+++ server-side/server.js
@@ -26,36 +26,148 @@ const server = express()
 
 
 
-
-
-
 const io = socketIO(server);
 var len=0;
-
+var usersarray={},usersobj={};
+var count={};//count for playagain button
 io.on('connection', function(socket){
+    var obj={};
     console.log('Client connected');
-
-    socket.on('createroom',function(room){
-        socket.join(room);
-        socket.emit('createres', room);
+    socket.on('createroom',function(data){
+        obj.name=data[1];
+        obj.id=socket;
+        socket.room=data[0];
+        count[data[0]]=1;
+        socket.username=data[1];
+        socket.join(data[0]);
+        socket.emit('createres', data[0]);
+        usersobj[data[0]]=[obj];
+        usersarray[data[0]]=[data[1]];
+        // console.log(usersobj);
+        // console.log(usersarray);
     });
 
-    socket.on('joinroom',function(room){
-        socket.join(room);
-        socket.emit('joinres',room);
-        // len=io.sockets.adapter.rooms[room].length;
+    socket.on('joinroom',function(data){
+        var len,obj={};
+        io.of('/').in(data[0]).clients(function(error,clients){
+            len=clients.length;
+        });
+        setTimeout(function() {
+            if(len == 0){
+                socket.emit('refusejoin',data[0]);
+            }else{
+                obj.name=data[1];
+                obj.id=socket;
+                socket.username=data[1];
+                socket.room=data[0];
+                socket.join(data[0]);
+                
+                usersobj[data[0]].push(obj);
+                usersarray[data[0]].push(data[1]);
+
+                socket.emit('joinres',usersarray[data[0]]);
+                socket.broadcast.to(data[0]).emit('users',usersarray[data[0]]);
+                // console.log(usersobj);
+                // console.log(usersarray);
+            }      
+        }, 1500);
     });
 
-    socket.on('forroommembers',function(data){
-        io.in(data[0]).emit('nextinput', data[1]);
+    socket.on('bingoinput',function(data){
+        socket.broadcast.to(data[0]).emit('bingoinput', data[1]);
+        var index=usersarray[data[0]].indexOf(data[2]);
+        if(index==(usersarray[data[0]].length)-1){
+            usersobj[data[0]][0].id.emit('nextchance',usersobj[data[0]][0].name);
+            io.in(data[0]).emit('showuserschance',usersobj[data[0]][0].name);
+        }else{
+            usersobj[data[0]][index+1].id.emit('nextchance',usersobj[data[0]][index+1].name);
+            io.in(data[0]).emit('showuserschance',usersobj[data[0]][index+1].name);
+        }
+        // console.log(usersobj[data[0]]);
     });
 
-    socket.on('resetgame',function(room){
-        io.in(room).emit('resetgame',"nothing");
+    socket.on('nextchanceonleave',function(data){
+        var index=usersarray[data[0]].indexOf(data[1]);
+        if(index==(usersarray[data[0]].length)-1){
+            usersobj[data[0]][0].id.emit('nextchance',usersobj[data[0]][0].name);
+            io.in(data[0]).emit('showuserschance',usersobj[data[0]][0].name);
+        }else{
+            usersobj[data[0]][index+1].id.emit('nextchance',usersobj[data[0]][index+1].name);
+            io.in(data[0]).emit('showuserschance',usersobj[data[0]][index+1].name);
+        }
+    });
+
+    socket.on('resetconfirm',function(room){
+        
+        var length;
+        io.of('/').in(room).clients(function(error,clients){
+            length=clients.length;
+        });
+        setTimeout(function() {
+            if(length==1){
+                socket.emit('resetgame',"nothing");
+            }
+        }, 1000);
+        count[room]=1;
+        socket.broadcast.to(room).emit('resetcount',"nothing");
+
+    });
+    socket.on('count',function(room){
+        var length;
+        count[room]++;
+        io.of('/').in(room).clients(function(error,clients){
+            length=clients.length;
+        });
+        setTimeout(function() {
+            if(length==count[room]){
+                io.in(room).emit('resetgame',"nothing");
+                count[room]=1;
+            }
+        }, 1000);
+
     });
 
     socket.on('chatmessage',function(data){
-        socket.broadcast.to(data[0]).emit('chatmessage', data[1]);
-    })
-    socket.on('disconnect', () => console.log('Client disconnected'));
+        socket.broadcast.to(data[0]).emit('chatmessage', data);
+    });
+    socket.on('leaveroom',function(data){
+        
+        var index=usersarray[data[0]].indexOf(data[1]);
+        usersarray[data[0]].splice(index,1);
+        usersobj[data[0]].splice(index,1);
+        if(usersarray[socket.room].length==0){
+            delete(usersarray[data[0]]);
+            delete(usersobj[data[0]]);
+            delete(count[data[0]]);
+        }else{
+            socket.broadcast.to(data[0]).emit('users',usersarray[data[0]]);
+        }
+        socket.leave(data[0]);
+        socket.room=null;
+        // console.log(usersobj);
+    });
+    socket.on('disconnect', function(){
+        console.log('Client disconnected');
+        if(socket.room != null){
+            var index=usersarray[socket.room].indexOf(socket.username);
+    //-----nextchance on disconnect----------------
+            if(index==(usersarray[socket.room].length)-1){
+                usersobj[socket.room][0].id.emit('nextchance',usersobj[socket.room][0].name);
+                io.in(socket.room).emit('showuserschance',usersobj[socket.room][0].name);
+            }else{
+                usersobj[socket.room][index+1].id.emit('nextchance',usersobj[socket.room][index+1].name);
+                io.in(socket.room).emit('showuserschance',usersobj[socket.room][index+1].name);
+            }
+    //-------------------------------------------
+            usersarray[socket.room].splice(index,1);
+            usersobj[socket.room].splice(index,1);
+            if(usersarray[socket.room].length==0){
+                delete(count[socket.room]);
+                delete(usersarray[socket.room]);
+                delete(usersobj[socket.room]);
+            }else{
+                io.in(socket.room).emit('users',usersarray[socket.room]);
+            }
+        }
+    });
 });
